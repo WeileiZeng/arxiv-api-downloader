6d83fb6edf1bd15364d7a786f39e437837121408
Weilei on HP workstation <weileizeng@zhejianglab.com>
2023-10-12 16:45:57 +0800

    download pdf file with progress bar

---
+++ util.py
@@ -0,0 +1,27 @@
+import requests
+from tqdm import tqdm
+import os
+
+def download(url: str, fname: str, chunk_size=1024):
+    resp = requests.get(url, stream=True)
+    total = int(resp.headers.get('content-length', 0))
+    tmp_file='tmp.pdf'
+    with open(tmp_file, 'wb') as file, tqdm(
+        desc=fname,
+        total=total,
+        unit='iB',
+        unit_scale=True,
+        unit_divisor=1024,
+    ) as bar:
+        for data in resp.iter_content(chunk_size=chunk_size):
+            size = file.write(data)
+            bar.update(size)
+
+    os.system(f'mv {tmp_file} {fname}')
+            
+def test():
+    url = 'http://arxiv.org/pdf/q-alg/9508008v1'
+    fname='q-alg.9508008v1.pdf'
+    download(url,fname)
+if __name__=="__main__":
+    test()
